b7d61950aa21deb9495ccd3ff27973fd01f1ea7a
dorakadri <kadridorra9@gmail.com>
2024-06-19 11:13:24 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./styles.css";
+
+import { FaReact } from "react-icons/fa";
+import Arborist from "./components/Arborist";
+import { content } from "./data/content";
+
+function App() {
+  return (
+    <div className="app-container">
+      <div className="sidebar">
+        <div className="heading-title">
+          <FaReact color="#00d8ff" size="32" />
+          <span>React-arborist</span>
+        </div>
+        <Arborist />
+      </div>
+      <div className="content">
+        <ul>
+          {content.map((item) => (
+            <li key={item}>{item}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
